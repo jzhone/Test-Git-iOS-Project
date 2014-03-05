5a0d1a446b718c4a2a8d36bb80b83d652d69b767
jzhone <jzhone@gmail.com>
2014-03-05 12:36:35 +0800

    add a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by jzhone on 14-3-5.
+//  Copyright (c) 2014年 jzhone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by jzhone on 14-3-5.
+//  Copyright (c) 2014年 jzhone. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
